417dda9c08f0ec3250a3d8321d1e4bb2f4f1f459
Arthur Guiot <arguiot@gmail.com>
2018-03-30 13:52:55 +0800

    Added basic files

---
+++ __test__/protype.js
@@ -0,0 +1,29 @@
+/*****************************************************
+
+	                    ProType
+	=================================================
+	Copyright © Arthur Guiot 2018. All right reserved.
+
+******************************************************/
+class ProType {
+	get Action() {
+		
+	}
+	constructor() {
+		this.version = "v0.0.1" // ProType version
+	}
+}
+// Browserify / Node.js
+if (typeof define === "function" && define.amd) {
+    define(() => new ProType);
+    // CommonJS and Node.js module support.
+} else if (typeof exports !== "undefined") {
+    // Support Node.js specific `module.exports` (which can be a function)
+    if (typeof module !== "undefined" && module.exports) {
+        exports = module.exports = new ProType;
+    }
+    // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+    exports.ProType = new ProType;
+} else if (typeof global !== "undefined") {
+    global.ProType = new ProType;
+}
