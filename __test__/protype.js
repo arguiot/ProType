3ce1a9e54253b33e313e4de8210286f057ad1aa5
arguiot <arguiot@gmail.com>
2018-04-28 10:07:38 +0800

    Fixing bugs

--- __test__/protype.js
+++ __test__/protype.js
@@ -16,6 +16,7 @@ class ProType {
 			performTransition(to, animation = "none", animTime = "1s", sender = "Any") {
 				const index = this.viewsName.indexOf(to)
 				const view = this.views[index]
+				view.setAttribute("style", "")
 				view.style["z-index"] = "-10"
 				view.style.display = "block"
 				this.view.style.animation = `${animation} ${animTime} forwards`;
--- src/functions/ViewController/performTransition.js
+++ src/functions/ViewController/performTransition.js
@@ -1,6 +1,7 @@
 performTransition(to, animation = "none", animTime = "1s", sender = "Any") {
 	const index = this.viewsName.indexOf(to)
 	const view = this.views[index]
+	view.setAttribute("style", "")
 	view.style["z-index"] = "-10"
 	view.style.display = "block"
 	this.view.style.animation = `${animation} ${animTime} forwards`;
