44471d72fc6e0d91e2d65c1f481d249fc17f2b89
arguiot <arguiot@gmail.com>
2018-05-01 18:00:18 +0800

    Updated the website

--- docs/js/functions.js
+++ docs/js/functions.js
@@ -5,6 +5,11 @@ class UIViewController extends P.ViewController {
 		this.year = this.view.querySelector("span.year")
 		this.year.innerHTML = new Date().getFullYear()
 		lunarIcons.replace()
+
+		let span = this.view.querySelector("span.rules")
+		let parent = span.parentNode.parentNode
+		const n = parent.querySelectorAll("h2").length
+		span.innerHTML = n
 	}
 }
 
