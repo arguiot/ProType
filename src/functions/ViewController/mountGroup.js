fdc09fd785cde609bd6e5135d6f49b967a48e309
arguiot <arguiot@gmail.com>
2018-05-12 16:12:54 +0800

    Added possibility to mount multiple groups

--- src/functions/ViewController/mountGroup.js
+++ src/functions/ViewController/mountGroup.js
@@ -2,3 +2,11 @@ mountGroup(el, ObjectClass) {
 	const obj = new ObjectClass(el, this.viewName)
 	return obj;
 }
+
+mountGroups(els, ObjectClass) {
+	let classes = []
+	for (let i = 0; i < els.length; i++) {
+		classes.push(new ObjectClass(els[i], this.viewName))
+	}
+	return classes
+}
